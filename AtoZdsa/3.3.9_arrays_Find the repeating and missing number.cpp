365fba572ec4901ada56331b44a2f5e25665aad7
rithesh-n <ritheshan18@gmail.com>
2025-01-15 04:07:52 +0530

    update

--- AtoZdsa/3.3.9_arrays_Find the repeating and missing number.cpp
+++ AtoZdsa/3.3.9_arrays_Find the repeating and missing number.cpp
@@ -22,9 +22,113 @@ class Solution {
         
         // The missing number is the difference between the expected sum and the actual sum,
         // adjusted by the repeating number.
-        long missing = expected_sum - (sum - repeating);
+        int missing = expected_sum - (sum - repeating);
         
         return { repeating,missing};
     }
     
 };
+
+// using hashing
+vector<int> findMissingRepeatingNumbers(vector<int> a) {
+    int n = a.size(); // size of the array
+    vector<int> hash(n + 1, 0); // hash array
+
+    //update the hash array:
+    for (int i = 0; i < n; i++) {
+        hash[a[i]]++;
+    }
+
+    //Find the repeating and missing number:
+    int repeating = -1, missing = -1;
+    for (int i = 1; i <= n; i++) {
+        if (hash[i] == 2) repeating = i;
+        else if (hash[i] == 0) missing = i;
+
+        if (repeating != -1 && missing != -1)
+            break;
+    }
+    return {repeating, missing};
+}
+
+//pure mathematics
+vector<int> findMissingRepeatingNumbers(vector<int> a) {
+    long long n = a.size(); // size of the array
+
+    // Find Sn and S2n:
+    long long SN = (n * (n + 1)) / 2;
+    long long S2N = (n * (n + 1) * (2 * n + 1)) / 6;
+
+    // Calculate S and S2:
+    long long S = 0, S2 = 0;
+    for (int i = 0; i < n; i++) {
+        S += a[i];
+        S2 += (long long)a[i] * (long long)a[i];
+    }
+
+    //S-Sn = X-Y:
+    long long val1 = S - SN;
+
+    // S2-S2n = X^2-Y^2:
+    long long val2 = S2 - S2N;
+
+    //Find X+Y = (X^2-Y^2)/(X-Y):
+    val2 = val2 / val1;
+
+    //Find X and Y: X = ((X+Y)+(X-Y))/2 and Y = X-(X-Y),
+    // Here, X-Y = val1 and X+Y = val2:
+    long long x = (val1 + val2) / 2;
+    long long y = x - val1;
+
+    return {(int)x, (int)y};
+}
+
+//Optimal Approach 2 (Using XOR): 
+vector<int> findMissingRepeatingNumbers(vector<int> a) {
+    int n = a.size(); // size of the array
+
+    int xr = 0;
+
+    //Step 1: Find XOR of all elements:
+    for (int i = 0; i < n; i++) {
+        xr = xr ^ a[i];
+        xr = xr ^ (i + 1);
+    }
+
+    //Step 2: Find the differentiating bit number:
+    int number = (xr & ~(xr - 1));
+
+    //Step 3: Group the numbers:
+    int zero = 0;
+    int one = 0;
+    for (int i = 0; i < n; i++) {
+        //part of 1 group:
+        if ((a[i] & number) != 0) {
+            one = one ^ a[i];
+        }
+        //part of 0 group:
+        else {
+            zero = zero ^ a[i];
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        //part of 1 group:
+        if ((i & number) != 0) {
+            one = one ^ i;
+        }
+        //part of 0 group:
+        else {
+            zero = zero ^ i;
+        }
+    }
+
+    // Last step: Identify the numbers:
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        if (a[i] == zero) cnt++;
+    }
+
+    if (cnt == 2) return {zero, one};
+    return {one, zero};
+}
