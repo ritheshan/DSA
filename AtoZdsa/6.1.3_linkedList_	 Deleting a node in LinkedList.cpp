ab00998ebd2dadc6fb205f1c4f46385ce4882ba2
rithesh-n <ritheshan18@gmail.com>
2025-01-21 23:12:02 +0530

    update

--- AtoZdsa/6.1.3_linkedList_	 Deleting a node in LinkedList.cpp
+++ AtoZdsa/6.1.3_linkedList_	 Deleting a node in LinkedList.cpp
@@ -1,14 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+
+//  // Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode(int x) : val(x), next(NULL) {}
+//  * };
+
 class Solution {
 public:
     void deleteNode(ListNode* node) {
@@ -17,3 +17,38 @@ public:
         
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
