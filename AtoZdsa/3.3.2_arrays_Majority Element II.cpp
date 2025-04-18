052c04c8057d8bac5f4254e995c87ac782aad474
rithesh-n <ritheshan18@gmail.com>
2025-04-19 03:19:18 +0530

    added the code for majority element"

---
+++ AtoZdsa/3.3.2_arrays_Majority Element II.cpp
@@ -0,0 +1,20 @@
+#include<vector>
+#include<unordered_map>
+using namespace std;
+class Solution {
+public:
+      
+    vector<int> majorityElement(vector<int>& nums) {
+        // if (nums.size()/3 < 1 ) return nums;
+        unordered_map<int, int> map;
+        for(int i=0;i<nums.size();i++){
+            map[nums[i]]+=1;
+        }
+        vector<int> ans;
+        for(auto [num , count]:map){
+            if(count>nums.size()/3) ans.push_back(num);
+        }
+
+        return ans;
+    }
+};
