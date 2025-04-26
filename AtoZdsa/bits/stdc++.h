d08ca95d9dfa026b61896cf8f648b60b29cdb034
rithesh-n <ritheshan18@gmail.com>
2025-04-27 00:19:58 +0530

    added the file to contail all the headers for c++ compiler execution

---
+++ AtoZdsa/bits/stdc++.h
@@ -0,0 +1,26 @@
+#ifndef _BITS_STDC_H
+#define _BITS_STDC_H
+
+// Include standard C++ headers
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <numeric>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <climits>
+#include <cstring>
+#include <iomanip>
+#include <functional>
+
+#endif
