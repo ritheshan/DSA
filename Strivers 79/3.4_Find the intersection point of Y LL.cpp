109060ef7f9381262914da622680e034a18dea48
rithesh-n <ritheshan18@gmail.com>
2025-06-14 07:37:23 +0530

    update

--- Strivers 79/3.4_Find the intersection point of Y LL.cpp
+++ Strivers 79/3.4_Find the intersection point of Y LL.cpp
@@ -26,3 +26,45 @@ class Solution {
             return nullptr; // No intersection
         }
     };
+// using difference of lengths of the two linked list
+   
+    int getDifference(ListNode* head1, ListNode* head2) {
+        int len1 = 0, len2 = 0;
+        while (head1 != NULL || head2 != NULL) {
+            if (head1 != NULL) {
+                ++len1; head1 = head1->next;
+            }
+            if (head2 != NULL) {
+                ++len2; head2 = head2->next;
+            }
+        }
+        return len1 - len2; // if difference is neg-> length of list2 > length of list1 else vice-versa
+    }
+   
+   //utility function to check presence of intersection
+ListNode* intersectionPresent(ListNode* head1, ListNode* head2) {
+    int diff = getDifference(head1,head2);
+           if(diff < 0) 
+               while(diff++ != 0) head2 = head2->next; 
+           else while(diff-- != 0) head1 = head1->next;
+           while(head1 != NULL) {
+               if(head1 == head2) return head1;
+               head2 = head2->next;
+               head1 = head1->next;
+           }
+           return head1;
+   
+   
+   }
+   
+
+// use of two pointers
+ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+    ListNode* a = headA;
+    ListNode* b = headB;
+    while (a != b) {
+        a = a == NULL ? headB : a->next;
+        b = b == NULL ? headA : b->next;
+    }
+    return a;
+}
