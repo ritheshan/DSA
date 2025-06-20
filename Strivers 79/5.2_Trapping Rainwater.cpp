7db6feb372386c5771d82d2c80a324c0092b0d36
rithesh-n <ritheshan18@gmail.com>
2025-06-20 12:12:46 +0530

    update

--- Strivers 79/5.2_Trapping Rainwater.cpp
+++ Strivers 79/5.2_Trapping Rainwater.cpp
@@ -26,3 +26,31 @@ class Solution {
             return total;
         }
     };
+
+// using two pointer approach
+
+int trap(vector < int > & height) {
+    int n = height.size();
+    int left = 0, right = n - 1;
+    int res = 0;
+    int maxLeft = 0, maxRight = 0;
+    while (left <= right) {
+      if (height[left] <= height[right]) {
+        if (height[left] >= maxLeft) {
+          maxLeft = height[left];
+        } else {
+          res += maxLeft - height[left];
+        }
+        left++;
+      } else {
+        if (height[right] >= maxRight) {
+          maxRight = height[right];
+        } else {
+          res += maxRight - height[right];
+        }
+        right--;
+      }
+    }
+    return res;
+  }
+  
