1433fa096e1a75c61d802844664881965f4b29f7
rithesh-n <ritheshan18@gmail.com>
2025-06-30 21:09:33 +0530

    added the ceil || floor solution

--- AtoZdsa/4.1.5_Ceil in Sorted Array.cpp
+++ AtoZdsa/4.1.5_Ceil in Sorted Array.cpp
@@ -24,3 +24,20 @@ public:
         return ans;
     }
 };
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low = 0, high = nums.size();
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid;
+        }
+
+        return low; // First index where nums[i] >= target
+    }
+};
