41c7b6eeb3b5f2bf96db209c152a8e7324d70e1b
rithesh-n <ritheshan18@gmail.com>
2025-07-19 00:09:37 +0530

    Add solution: 4.3.3 Search in row and column wise sorted matrix

---
+++ AtoZdsa/4.3.3_Search in a row and column wise sorted matrix.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int row = 0, col = n - 1;
+
+        while (row < m && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] > target) {
+                col--; // move left
+            } else {
+                row++; // move down
+            }
+        }
+
+        return false;
+    }
+};
