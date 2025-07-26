bf81e9f5b54cda890efa6d3e07f1504c74cbea1f
rithesh-n <ritheshan18@gmail.com>
2025-07-27 00:19:48 +0530

    added the solution for the upper bound challenge

--- AtoZdsa/4.1.3_Implement Upper Bound.cpp
+++ AtoZdsa/4.1.3_Implement Upper Bound.cpp
@@ -3,21 +3,6 @@ using namespace std;
 
 class Solution {
 public:
-    int lowerBound(vector<int>& nums, int x) {
-        int n = nums.size();
-        int low = 0, high = n;
-
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] < x)
-                low = mid + 1;
-            else
-                high = mid;
-        }
-
-        return low;  // First index where nums[i] >= x
-    }
     int upperBound(vector<int>& nums, int x) {
         int n = nums.size();
         int low = 0, high = n;
