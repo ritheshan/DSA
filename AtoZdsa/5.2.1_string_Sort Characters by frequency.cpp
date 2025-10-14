c0f4577e6dcc570337d71ae574876f914955be92
rithesh-n <ritheshan18@gmail.com>
2025-10-15 01:41:07 +0530

    Add solution to sort characters by frequency in a string

---
+++ AtoZdsa/5.2.1_string_Sort Characters by frequency.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int> mp;
+        multimap<int,char> r;
+        string ss="";
+
+        for(auto a : s)
+            mp[a]++;
+
+        for(auto a : mp)
+            r.insert({a.second, a.first});
+
+        for(auto it = r.rbegin(); it != r.rend(); ++it)
+            ss += string(it->first, it->second);
+
+        //for(auto it = r.rbegin(); it != r.rend(); ++it){
+        //   for (int i = 0; i < it->first; ++i) {
+        //          ss += it->second;
+        //        }
+        //} 
+
+        return ss;
+    }
+};
+
