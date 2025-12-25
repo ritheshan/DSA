a2b507615ae4db3ad71f6d6a9368e2686c3da994
rithesh-n <ritheshan18@gmail.com>
2025-12-26 03:16:35 +0530

    added the solution to the maximize happiness of selected children

---
+++ LEETCODE/3075/3075. Maximize Happiness of Selected Children.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        sort(happiness.begin(), happiness.end(), greater<int>());
+
+        long long ans = 0;
+        for (int i = 0; i < k; i++) {
+            int curr = happiness[i] - i;
+            if (curr <= 0) break;
+            ans += curr;
+        }
+        return ans;
+    }
+};
